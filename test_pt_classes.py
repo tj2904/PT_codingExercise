85380fad0543bb07dd7d53f3260cfedc9ee287b3
Tim Jackson <tj2904+github@gmail.com>
2021-07-27 17:30:54 +0100

    seperated out functions & added 2 students

--- test_pt_classes.py
+++ test_pt_classes.py
@@ -3,7 +3,7 @@ import pt_classes
 
 class tests(unittest.TestCase):
     
-    def test_pt_classes(self):
+    def test_wholeClass(self):
         Student0080 = pt_classes.Student(99,88,92,96,79,54)
         self.assertEqual(Student0080.averageActual(), 79.33333333333333, "Should be 79.33333333333333")
         self.assertEqual(Student0080.progress()[0], -1.1, "Maths Progress: should return -1.1")
@@ -17,6 +17,17 @@ class tests(unittest.TestCase):
         self.assertEqual(Student6911.progress()[1], -1.5, "English Progress: should return -1.5")
         self.assertEqual(Student6911.progress()[2], -1.6, "Science Progress: should return -1.6")
         self.assertEqual(Student6911.progress()[3], 0.3666666666666667, "Average Progress: should return 0.366...7")
+
+    def test_averageActual(self):
+        Student1010 = pt_classes.Student(64,75,62,96,53,92)
+        self.assertEqual(Student1010.averageActual(), 87.66666666666667, "Should be 87.67")
+
+    def test_progress(self):
+        Student9729 = pt_classes.Student(78,50,67,78,93,54)
+        self.assertEqual(Student9729.progress()[0], -2.8, "Maths Progress: should return -2.8")
+        self.assertEqual(Student9729.progress()[1], 1.1, "English Progress: should return 1.1")
+        self.assertEqual(Student9729.progress()[2], -3.9, "Science Progress: should return -3.9")
+        self.assertEqual(Student9729.progress()[3], -1.8666666666666665, "Average Progress: should return -1.87")
 
 
 if __name__ == "__main__":
